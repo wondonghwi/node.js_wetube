0ad550dd5812d4deabefc842b694715215707298
wondonghwi <donghwi1289@gmail.com>
2021-10-01 01:58:39 +0900

    add : express (req, res) 작업

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send('study');
+};
+
+app.get('/', handleHome);
+
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
