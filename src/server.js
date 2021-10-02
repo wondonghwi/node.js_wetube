58b02f825478d6115c8ca26a615caa2d4b8ca716
wondonghwi <donghwi1289@gmail.com>
2021-10-02 09:55:35 +0900

    add : morgan 라이브러리 설치 후 미들웨어 적용

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan('dev');
 
 const handleHome = (req, res) => {
-  return res.send('study');
+  return res.send('home');
 };
 
+const handleLogin = (req, res) => {
+  return res.send('login');
+};
+
+app.use(logger); //app.use는 항상 최상위 위치에 있어야함 global
 app.get('/', handleHome);
+app.get('/login', handleLogin);
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
