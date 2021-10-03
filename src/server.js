1ed997f160221c5850cfdf2d9ab7221999cee208
wondonghwi <donghwi1289@gmail.com>
2021-10-04 00:06:03 +0900

    add : pug파일 공통파일만들어서 사용 작업 진행

--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
-
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
 const PORT = 4000;
-
 const app = express();
 const logger = morgan('dev');
 
 app.set('view engine', 'pug');
-app.use(logger); //app.use는 항상 최상위 위치에 있어야함 global
+app.set('views', process.cwd() + '/src/views');
+app.use(logger);
 app.use('/', globalRouter);
-app.use('/users', userRouter);
 app.use('/videos', videoRouter);
+app.use('/users', userRouter);
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
