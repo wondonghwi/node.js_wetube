c8f2064fd24adfdbcbd57707ce71ada0bec0f39c
wondonghwi <donghwi1289@gmail.com>
2021-10-06 22:43:21 +0900

    add : upload관련 get , post Controller 함수구현

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getEdit, postEdit, watch } from './controllers/videoController';
+import { getEdit, getUpload, postEdit, postUpload, watch } from './controllers/videoController';
 
 const videoRouter = express.Router();
 
@@ -7,5 +7,6 @@ videoRouter.get('/:id(\\d+)', watch);
 // videoRouter.get('/:id(\\d+)/edit', getEdit);
 // videoRouter.post('/:id(\\d+)/edit', postEdit);
 videoRouter.route('/:id(\\d+)/edit').get(getEdit).post(postEdit);
+videoRouter.route('/upload').get(getUpload).post(postUpload);
 
 export default videoRouter;
