ad28d5ad243957e0a3e65183b5de6f915f62fb61
wondonghwi <donghwi1289@gmail.com>
2021-10-12 00:02:08 +0900

    add : findByIdAndDelete를 이용한 DB에 데이터 삭제 구현

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getEdit, getUpload, postEdit, postUpload, watch } from './controllers/videoController';
+import { deleteVideo, getEdit, getUpload, postEdit, postUpload, watch } from './controllers/videoController';
 
 const videoRouter = express.Router();
 
@@ -7,6 +7,7 @@ videoRouter.get('/:id([0-9a-f]{24})', watch);
 // videoRouter.get('/:id([0-9a-f]{24})/edit', getEdit);
 // videoRouter.post('/:id([0-9a-f]{24})/edit', postEdit);
 videoRouter.route('/:id([0-9a-f]{24})/edit').get(getEdit).post(postEdit);
+videoRouter.route('/:id([0-9a-f]{24})/delete').get(deleteVideo);
 videoRouter.route('/upload').get(getUpload).post(postUpload);
 
 export default videoRouter;
