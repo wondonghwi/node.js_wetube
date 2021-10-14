c06d1ee01756360c2e34255539a0e7b1baf9669b
wondonghwi <donghwi1289@gmail.com>
2021-10-14 21:10:53 +0900

    add : login 기능, login id가 존재하는지 exists 확인 진행

--- src/routers/rootlRouter.js
+++ src/routers/rootlRouter.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { home, search } from './controllers/videoController';
-import { getJoin, login, postJoin } from './controllers/userController';
+import { getJoin, getLogin, postJoin, postLogin } from './controllers/userController';
 
 const rootRouter = express.Router();
 
 rootRouter.get('/', home);
 rootRouter.route('/join').get(getJoin).post(postJoin);
-rootRouter.get('/login', login);
+rootRouter.route('/login').get(getLogin).post(postLogin);
 rootRouter.get('/search', search);
 
 export default rootRouter;
