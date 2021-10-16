b0bb8751785d6cc07a077443b8c6b36b04cf3d05
wondonghwi <donghwi1289@gmail.com>
2021-10-16 23:33:33 +0900

    add : Oauth2 -> github 로그인구현, redirect주소 정의

--- src/routers/controllers/userController.js
+++ src/routers/controllers/userController.js
@@ -59,6 +59,20 @@ export const postLogin = async (req, res) => {
   return res.redirect('/');
 };
 
+export const startGithublogin = (req, res) => {
+  const baseUrl = 'https://github.com/login/oauth/authorize';
+  const config = {
+    client_id: '0ec238fed625d798b0ee',
+    allow_signup: false,
+    scope: 'read:user user:email',
+  };
+  const params = new URLSearchParams(config).toString();
+  const finishUrl = `${baseUrl}?${params}`;
+  return res.redirect(finishUrl);
+};
+
+// export const finishGithubLogin = (req, res) => {};
+
 export const edit = (req, res) => res.send('Edit User');
 export const remove = (req, res) => res.send('Remove User');
 export const logout = (req, res) => res.send('Log out');
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { edit, remove } from './controllers/userController';
+import { edit, finishGithubLogin, logout, remove, see, startGithublogin } from './controllers/userController';
 
 const userRouter = express.Router();
 
+userRouter.get('/logout', logout);
 userRouter.get('/edit', edit);
 userRouter.get('/remove', remove);
+userRouter.get('/edit', edit);
+userRouter.get('/github/start', startGithublogin);
+// userRouter.get('/github/finish', finishGithubLogin);
+userRouter.get(':id', see);
 
 export default userRouter;
