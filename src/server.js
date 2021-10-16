0a876ce2a42f96f5c290eb1309184c790f85e4e1
wondonghwi <donghwi1289@gmail.com>
2021-10-16 17:06:56 +0900

    add : 로그인된 사용자의 쿠키만 받아와서 저장하도록 수정 작업진행

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import rootRouter from './routers/rootlRouter';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
@@ -16,8 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: 'Hello!',
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/wetube' }),
   })
 );
 
