1f4ed7b649cf8bcb35db7cddba37c1efe4dd3680
wondonghwi <donghwi1289@gmail.com>
2021-10-18 23:33:20 +0900

    add : Edit-Profile 변경시 프론트 화면에도 적용되도록 수정작업진행

--- src/routers/controllers/userController.js
+++ src/routers/controllers/userController.js
@@ -129,8 +129,26 @@ export const logout = (req, res) => {
 export const getEdit = (req, res) => {
   return res.render('edit-profile', { pageTitle: 'Edit Profile' });
 };
-export const postEdit = (req, res) => {
-  return res.render('edit-profile');
+
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updatedUser;
+  return res.redirect('/users/edit');
 };
 
 export const see = (req, res) => res.send('See User');
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { deleteVideo, getEdit, getUpload, postEdit, postUpload, watch } from './controllers/videoController';
+import { protectorMiddleware } from '../middlewares';
 
 const videoRouter = express.Router();
 
 videoRouter.get('/:id([0-9a-f]{24})', watch);
 // videoRouter.get('/:id([0-9a-f]{24})/edit', getEdit);
 // videoRouter.post('/:id([0-9a-f]{24})/edit', postEdit);
-videoRouter.route('/:id([0-9a-f]{24})/edit').get(getEdit).post(postEdit);
-videoRouter.route('/:id([0-9a-f]{24})/delete').get(deleteVideo);
-videoRouter.route('/upload').get(getUpload).post(postUpload);
+videoRouter.route('/:id([0-9a-f]{24})/edit').all(protectorMiddleware).get(getEdit).post(postEdit);
+videoRouter.route('/:id([0-9a-f]{24})/delete').all(protectorMiddleware).get(deleteVideo);
+videoRouter.route('/upload').all(protectorMiddleware).get(getUpload).post(postUpload);
 
 export default videoRouter;
