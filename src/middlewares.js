193f462f371c343f7ee2f32e75b30f2c1e5c686c
wondonghwi <donghwi1289@gmail.com>
2021-10-18 22:35:41 +0900

    add : Edit-Profile 미들웨어 작성

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,23 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = !!req.session.loggedIn;
   res.locals.siteName = 'Wetube';
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
+  console.log(res.locals.loggedInUser);
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect('/login');
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect('/');
+  }
 };
