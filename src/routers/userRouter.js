8f4de9a3c1a3135e438563df086fbfa24a6da29f
wondonghwi <donghwi1289@gmail.com>
2021-10-18 23:50:39 +0900

    add : change-password 구현

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,21 @@
 import express from 'express';
-import { finishGithubLogin, getEdit, logout, postEdit, see, startGithubLogin } from './controllers/userController';
+import {
+  finishGithubLogin,
+  getChangePassword,
+  getEdit,
+  logout,
+  postChangePassword,
+  postEdit,
+  see,
+  startGithubLogin,
+} from './controllers/userController';
 import { protectorMiddleware, publicOnlyMiddleware } from '../middlewares';
 
 const userRouter = express.Router();
 
 userRouter.get('/logout', protectorMiddleware, logout);
 userRouter.route('/edit').all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.route('/change-password').all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin);
 userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(':id', see);
