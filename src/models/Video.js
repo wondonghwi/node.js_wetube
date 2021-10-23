0680e308a3a6a75cc56c3a2d336dee5c685a3031
wondonghwi <donghwi1289@gmail.com>
2021-10-23 23:12:20 +0900

    add : Video를 어떤 User가 올렸는지 Video와 User를 연결시키지는 작업 진행

--- src/models/Video.js
+++ src/models/Video.js
@@ -10,6 +10,7 @@ const videoSchema = new mongoose.Schema({
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
   },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
 });
 
 //커스텀 static function
--- src/routers/controllers/videoController.js
+++ src/routers/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from '../../models/Video';
+import User from '../../models/User';
 
 export const home = async (req, res) => {
   //TODO 콜백 처리방식
@@ -19,10 +20,11 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  const owner = await User.findById(video.owner);
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found.' });
   }
-  return res.render('watch', { pageTitle: video.title, video });
+  return res.render('watch', { pageTitle: video.title, video, owner });
 };
 
 export const getEdit = async (req, res) => {
@@ -54,6 +56,9 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+  const {
+    user: { _id },
+  } = req.session;
   const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
@@ -61,6 +66,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       fileUrl,
+      owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect('/');
