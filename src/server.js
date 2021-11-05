25eecebf00242b002c47337eccba10a2da599022
wondonghwi <donghwi1289@gmail.com>
2021-11-06 08:54:12 +0900

    add :썸네일 추가 , 파일다운로드기능 추가

--- src/server.js
+++ src/server.js
@@ -11,6 +11,12 @@ import { localsMiddleware } from './middlewares';
 const app = express();
 const logger = morgan('dev');
 
+app.use((req, res, next) => {
+  res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.header('Cross-Origin-Opener-Policy', 'same-origin');
+  next();
+});
+
 app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 app.use(logger);
