5acd327dd184adcecb5b9a7b2da4f0d287409af6
wondonghwi <donghwi1289@gmail.com>
2021-11-06 09:08:22 +0900

    add : apply flash message

--- src/routers/controllers/userController.js
+++ src/routers/controllers/userController.js
@@ -123,6 +123,7 @@ export const finishGithubLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
+  req.flash('info', 'Bye Bye');
   return res.redirect('/');
 };
 
@@ -155,6 +156,7 @@ export const postEdit = async (req, res) => {
 
 export const getChangePassword = (req, res) => {
   if (req.session.user.socialOnly === true) {
+    req.flash('error', "Can't change password.");
     return res.redirect('/');
   }
   return res.render('users/change-password', { pageTitle: 'Change Password' });
@@ -182,6 +184,7 @@ export const postChangePassword = async (req, res) => {
   }
   user.password = newPassword;
   await user.save();
+  req.flash('info', 'Password updated');
   return res.redirect('/users/logout');
 };
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import session from 'express-session';
+import flash from 'express-flash';
 import MongoStore from 'connect-mongo';
 import rootRouter from './routers/rootlRouter';
 import videoRouter from './routers/videoRouter';
@@ -34,6 +35,7 @@ app.use(
   })
 );
 
+app.use(flash());
 app.use(localsMiddleware);
 app.use('/uploads', express.static('uploads'));
 app.use('/static', express.static('assets'));
