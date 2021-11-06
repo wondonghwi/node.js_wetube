38182e855c23e0e72bbb159efa86b87ce6367760
wondonghwi <donghwi1289@gmail.com>
2021-11-06 10:40:48 +0900

    add : vedio Edit , Delete등 기존 기능 활성화

--- src/routers/controllers/videoController.js
+++ src/routers/controllers/videoController.js
@@ -1,4 +1,6 @@
 import Video from '../../models/Video';
+import Comment from '../../models/Comment';
+import User from '../../models/User';
 
 export const home = async (req, res) => {
   //TODO 콜백 처리방식
@@ -18,7 +20,8 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate('owner');
+  const video = await Video.findById(id).populate('owner').populate('comments');
+  console.log(video);
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found.' });
   }
@@ -134,4 +137,24 @@ export const registerView = async (req, res) => {
   video.meta.views = video.meta.views + 1;
   await video.save();
   return res.sendStatus(200);
+};
+
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+  video.comments.push(comment._id);
+  video.save();
+  return res.status(201).json({ newCommentId: comment._id });
 };
--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(
   session({
